1508c6a63a89b9cb045eca7f206d5d26b37fe546
MaxAlmeida <llewxam100@hotmail.com>
2015-04-16 14:53:18 -0300

    fixing route in statistic

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -3,7 +3,7 @@ class StatisticsController < ApplicationController
   	metric_configuration = MetricConfiguration.all
   	
   	count_metric_name = Hash.new(0)
-  	metric.each { |x|  count_metric_name[x.metric_snapshot.name]+=1 }
+  	metric_configuration.each { |x|  count_metric_name[x.metric_snapshot.name]+=1 }
 
     respond_to do |format|
      	format.json {render json: {count_metric_name: count_metric_name}
